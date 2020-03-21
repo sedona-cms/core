706c706de50b896b6efe36c7dec7b5ed74aaa04b
Ivan Kalinichenko <kalinichenko@constlab.ru>
2020-03-21 16:22:51 +0300

    feature: save open state

--- src/components/router-view/router-panel.tsx
+++ src/components/router-view/router-panel.tsx
@@ -1,6 +1,8 @@
 import Vue, { VNode } from 'vue'
 import RouterView from './router-view'
 
+let qApp: HTMLElement | null
+
 export default Vue.extend({
     name: 'RouterPanel',
     components: {
@@ -11,12 +13,30 @@ export default Vue.extend({
             isPanelOpen: false as Boolean,
         }
     },
+    async mounted(): Promise<void> {
+        await this.$nextTick()
+
+        qApp = document.getElementById('q-app')
+
+        const isOpen: boolean = JSON.parse(localStorage.getItem('sedona-panel-open') || 'false')
+        if (isOpen) {
+            this.open()
+        }
+    },
     methods: {
         toggle: function (): void {
-            const qApp = document.getElementById('q-app')
             if (qApp !== null) {
-                qApp.style.left = qApp.style.left === '-300px' ? '0' : '-300px'
+                qApp.style.left = qApp.style.left === '-300px' ? '0px' : '-300px'
                 this.isPanelOpen = qApp.style.left === '0px'
+                localStorage.setItem('sedona-panel-open', String(this.isPanelOpen))
+            }
+
+        },
+        open(): void {
+            if (qApp !== null) {
+                qApp.style.left = '0px'
+                this.isPanelOpen = true
+                localStorage.setItem('sedona-panel-open', String(true))
             }
         },
     },
@@ -26,7 +46,7 @@ export default Vue.extend({
                 <div class="admin-panel--inner fit text-white q-gutter-y-sm shadow-5" style="z-index:1000">
                     <q-btn
                         color="dark"
-                        icon={this.isPanelOpen ? 'menu_open' : 'menu'}
+                        icon={ this.isPanelOpen ? 'menu_open' : 'menu' }
                         round={ true }
                         class="toggle-button"
                         on-click={ this.toggle }
