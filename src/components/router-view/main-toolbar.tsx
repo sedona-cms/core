6c8e265dad975b9d0b655b9d4b27ac3aab439fec
Ivan Kalinichenko <kalinichenko@constlab.ru>
2020-03-23 14:38:50 +0300

    chore: git commit hook test

--- src/components/router-view/main-toolbar.tsx
+++ src/components/router-view/main-toolbar.tsx
@@ -2,48 +2,48 @@ import Vue, { VNode } from 'vue'
 import { uid } from 'quasar'
 
 export default Vue.extend({
-    name: 'MainToolbar',
-    methods: {
-        buttonClick({ id, title, component, icon = 'folder' }) {
-            console.log(id, title, component)
-            // this.$admin.goTo({ id, title, icon, component })
-        },
+  name: 'MainToolbar',
+  methods: {
+    buttonClick({ id, title, component, icon = 'folder' }) {
+      console.log(id, title, component, icon)
+      // this.$admin.goTo({ id, title, icon, component })
     },
-    render(): VNode {
-        const buttons: VNode[] = []
+  },
+  render(): VNode {
+    const buttons: VNode[] = []
 
-        if (this.$sedona.toolBarConfig.showHome) {
-            buttons.push(
-                <q-btn flat={ true } round={ true } dense={ true } class="q-mr-sm">
-                    <q-icon name="home"/>
-                    <q-tooltip content-class="bg-amber text-black shadow-4">Home</q-tooltip>
-                </q-btn>,
-            )
-        }
+    if (this.$sedona.toolBarConfig.showHome) {
+      buttons.push(
+        <q-btn flat={true} round={true} dense={true} class="q-mr-sm">
+          <q-icon name="home" />
+          <q-tooltip content-class="bg-amber text-black shadow-4">Home</q-tooltip>
+        </q-btn>
+      )
+    }
 
-        this.$sedona.toolBarConfig.buttons.forEach((button: ToolbarButton) => {
-            const title = button?.title || false
-            const icon = button?.icon || 'extension'
-            const id = uid()
+    this.$sedona.toolBarConfig.buttons.forEach((button: ToolbarButton) => {
+      const title = button?.title || false
+      const icon = button?.icon || 'extension'
+      const id = uid()
 
-            buttons.push(
-                <q-btn
-                    flat={ true }
-                    round={ true }
-                    dense={ true }
-                    class="q-mr-sm"
-                    on-click={ () => this.buttonClick({ id, ...button }) }
-                >
-                    <q-icon name={ icon }/>
-                    { title ? <q-tooltip>{ button.title }</q-tooltip> : '' }
-                </q-btn>,
-            )
-        })
+      buttons.push(
+        <q-btn
+          flat={true}
+          round={true}
+          dense={true}
+          class="q-mr-sm"
+          on-click={() => this.buttonClick({ id, ...button })}
+        >
+          <q-icon name={icon} />
+          {title ? <q-tooltip>{button.title}</q-tooltip> : ''}
+        </q-btn>
+      )
+    })
 
-        if (this.$sedona.toolBarConfig.title !== '') {
-            buttons.push(<q-toolbar-title>{ this.$sedona.toolBarConfig.title }</q-toolbar-title>)
-        }
+    if (this.$sedona.toolBarConfig.title !== '') {
+      buttons.push(<q-toolbar-title>{this.$sedona.toolBarConfig.title}</q-toolbar-title>)
+    }
 
-        return <q-toolbar class="bg-grey-7">{ buttons }</q-toolbar>
-    },
+    return <q-toolbar class="bg-grey-7">{buttons}</q-toolbar>
+  },
 })
--- src/templates/sedona.ts
+++ src/templates/sedona.ts
@@ -1,24 +1,24 @@
 export class Sedona {
-    private readonly config: ModuleConfig
+  private readonly config: ModuleConfig
 
-    constructor(config: ModuleConfig) {
-        this.config = Object.freeze(config)
-    }
+  constructor(config: ModuleConfig) {
+    this.config = Object.freeze(config)
+  }
 
-    get menuItems(): MenuItem[] {
-        return this.config?.items || []
-    }
+  get menuItems(): MenuItem[] {
+    return this.config?.items || []
+  }
 
-    get toolBarConfig(): ToolbarConfig {
-        const defaultToolbarConfig: ToolbarConfig = {
-            showHome: true,
-            title: '',
-            buttons: [],
-        }
-        return Object.assign({}, defaultToolbarConfig, this.config?.toolbar || {})
+  get toolBarConfig(): ToolbarConfig {
+    const defaultToolbarConfig: ToolbarConfig = {
+      showHome: true,
+      title: '',
+      buttons: [],
     }
+    return Object.assign({}, defaultToolbarConfig, this.config?.toolbar || {})
+  }
 
-    goBack(): void {
-        console.log('goBack hook')
-    }
+  goBack(): void {
+    console.log('goBack hook!')
+  }
 }
