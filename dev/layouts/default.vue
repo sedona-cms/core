db182736f9e86e27a8f18babdd04f9a958b6c555
Ivan Kalinichenko <kalinichenko@constlab.ru>
2020-03-23 14:46:58 +0300

    chore: test commit hook

--- dev/layouts/default.vue
+++ dev/layouts/default.vue
@@ -1,17 +1,17 @@
 <template>
-    <div>
-        <a href="#" @click.prevent="load">Load admin</a>
-        <nuxt />
-    </div>
+  <div>
+    <a href="#" @click.prevent="load">Load admin panel</a>
+    <nuxt />
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'DefaultLayout',
-        methods: {
-          async load() {
-              await this.$adminLoader.load()
-          }
-        },
-    }
+  export default {
+    name: 'DefaultLayout',
+    methods: {
+      async load() {
+        await this.$adminLoader.load()
+      },
+    },
+  }
 </script>
