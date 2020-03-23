63b7441c68fa4bc8c118efc6997e06b2df2452b0
Ivan Kalinichenko <kalinichenko@constlab.ru>
2020-03-23 14:36:50 +0300

    chore: test commit

--- src/module.ts
+++ src/module.ts
@@ -6,17 +6,17 @@ import { loadConfigFile } from './utils/config'
 export const meta = require('../package.json')
 
 const defaultOptions: ModuleConfig = {
-    items: [],
+  items: [],
 }
 
 const adminModule: Module<ModuleConfig> = async function (moduleOptions) {
-    this.extendBuild((config) => {
-        config.node = {
-            fs: 'empty',
-        }
-    })
+  this.extendBuild(config => {
+    config.node = {
+      fs: 'empty',
+    }
+  })
 
-    /* if (this.options.build === undefined) {
+  /* if (this.options.build === undefined) {
         this.options.build = {
             transpile: [ meta.name ],
         }
@@ -26,52 +26,53 @@ const adminModule: Module<ModuleConfig> = async function (moduleOptions) {
         this.options.build.transpile = [ meta.name ]
     } */
 
+  // @ts-ignore
+  if (Array.isArray(this.options.build.transpile)) {
     // @ts-ignore
-    if (Array.isArray(this.options.build.transpile)) {
-        // @ts-ignore
-        this.options.build.transpile.push(meta.name)
-    } else {
-        // @ts-ignore
-        this.options.build.transpile = [ meta.name ]
+    this.options.build.transpile.push(meta.name)
+  } else {
+    // @ts-ignore
+    this.options.build.transpile = [meta.name]
+  }
+
+  let options: ModuleConfig
+  if (moduleOptions === undefined || (typeof moduleOptions === 'object' && Object.keys(moduleOptions).length === 0)) {
+    options = await loadConfigFile(this.options.rootDir || process.cwd())
+    options = Object.assign({}, defaultOptions, options)
+  } else {
+    options = Object.assign({}, defaultOptions, moduleOptions)
+  }
+
+  options.items = options.items.map(item => {
+    return {
+      id: uid(),
+      ...item,
     }
+  })
 
-    let options: ModuleConfig
-    if (moduleOptions === undefined || (typeof moduleOptions === 'object' && Object.keys(moduleOptions).length === 0)) {
-        options = await loadConfigFile(this.options.rootDir || process.cwd())
-        options = Object.assign({}, defaultOptions, options)
-    } else {
-        options = Object.assign({}, defaultOptions, moduleOptions)
-    }
+  this.addTemplate({
+    src: path.resolve(__dirname, 'templates/quasar.js'),
+    fileName: path.join('nuxt-admin', 'quasar.js'),
+  })
 
-    options.items = options.items.map(item => {
-        return {
-            id: uid(),
-            ...item,
-        }
-    })
+  this.addTemplate({
+    src: path.resolve(__dirname, 'templates/admin-loader.js'),
+    fileName: path.join('nuxt-admin', 'admin-loader.js'),
+  })
 
-    this.addTemplate({
-        src: path.resolve(__dirname, 'templates/quasar.js'),
-        fileName: path.join('nuxt-admin', 'quasar.js'),
-    })
-    this.addTemplate({
-        src: path.resolve(__dirname, 'templates/admin-loader.js'),
-        fileName: path.join('nuxt-admin', 'admin-loader.js'),
-    })
-    this.addTemplate({
-        src: path.resolve(__dirname, 'templates/sedona.js'),
-        fileName: path.join('nuxt-admin', 'sedona.js'),
-    })
+  this.addTemplate({
+    src: path.resolve(__dirname, 'templates/sedona.js'),
+    fileName: path.join('nuxt-admin', 'sedona.js'),
+  })
 
+  // Plugins
 
-    // Plugins
-
-    this.addPlugin({
-        src: path.resolve(__dirname, 'templates/plugin.js'),
-        fileName: path.join('nuxt-admin', 'plugin.admin.js'),
-        mode: 'client',
-        options,
-    })
+  this.addPlugin({
+    src: path.resolve(__dirname, 'templates/plugin.js'),
+    fileName: path.join('nuxt-admin', 'plugin.admin.js'),
+    mode: 'client',
+    options,
+  })
 }
 
 export default adminModule
