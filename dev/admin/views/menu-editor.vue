cdd5f2fe0b0650d2664ac503cfc2d54c5b40de66
Ivan Kalinichenko <kalinichenko@constlab.ru>
2020-05-01 20:30:19 +0300

    wip: add link-editor in demo

---
+++ dev/admin/views/menu-editor.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <links-editor :items="[]" />
+  </div>
+</template>
+
+<script>
+  import { LinksEditor } from '@sedona-cms/core/lib/components'
+
+  export default {
+    name: 'MenuEditor',
+    components: {
+      LinksEditor,
+    },
+  }
+</script>
