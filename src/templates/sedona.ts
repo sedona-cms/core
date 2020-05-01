429a6a69abe553e28c4f48f63e647dd361ba6aae
Ivan Kalinichenko <kalinichenko@constlab.ru>
2020-05-01 20:22:19 +0300

    refactor: set sedona config readonly

--- src/templates/sedona.ts
+++ src/templates/sedona.ts
@@ -1,17 +1,17 @@
 const config: Readonly<ModuleConfig> = Object.freeze(JSON.parse('<%= JSON.stringify(options) %>'))
 
 export class Sedona {
-  get menuItems(): MenuItem[] {
-    return config?.items || []
+  get menuItems(): readonly MenuItem[] {
+    return Object.freeze(config?.items || [])
   }
 
-  get toolBarConfig(): ToolbarConfig {
+  get toolBarConfig(): Readonly<ToolbarConfig> {
     const defaultToolbarConfig: ToolbarConfig = {
       showHome: true,
       title: '',
       buttons: [],
     }
-    return Object.assign({}, defaultToolbarConfig, config?.toolbar || {})
+    return Object.freeze(Object.assign({}, defaultToolbarConfig, config?.toolbar || {}))
   }
 
   goBack(): void {
