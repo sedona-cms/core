0fa1909eb7775a7db977ef4e0a9598394c45c726
Ivan Kalinichenko <kalinichenko@constlab.ru>
2020-05-12 10:52:44 +0300

    wip: add QSelect

--- src/templates/quasar.ts
+++ src/templates/quasar.ts
@@ -25,6 +25,7 @@ import { QCheckbox } from 'quasar/src/components/checkbox'
 import { QField } from 'quasar/src/components/field'
 import { QDate } from 'quasar/src/components/date'
 import { QEditor } from 'quasar/src/components/editor'
+import { QSelect } from 'quasar/src/components/select'
 
 import { QCircularProgress } from 'quasar/src/components/circular-progress'
 import { QMenu } from 'quasar/src/components/menu'
@@ -71,6 +72,7 @@ export default {
         QVideo,
         QUploader,
         QPagination,
+        QSelect,
       },
       directives,
       plugins: {},
